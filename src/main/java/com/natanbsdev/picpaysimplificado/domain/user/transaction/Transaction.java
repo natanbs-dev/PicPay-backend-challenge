878f9a47af6b41304a6a5327db1af4bf4905cc78
barbosa-kde <natanbarbosa50@gmail.com>
2024-11-18 15:08:52 -0300

    feat: configurando o transaction do USER

---
+++ src/main/java/com/natanbsdev/picpaysimplificado/domain/user/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.natanbsdev.picpaysimplificado.domain.user.transaction;
+
+
+import com.natanbsdev.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver")
+    private User receiver;
+
+    //quando foi realizada a nossa transação
+    private LocalDateTime timestamp;
+}
