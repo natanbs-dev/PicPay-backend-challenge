88651c68322173ab31e3de56cd1ea9de3d705ce3
barbosa-kde <natanbarbosa50@gmail.com>
2024-11-18 17:55:15 -0300

    feat: serviço de notificação

---
+++ src/main/java/com/natanbsdev/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,30 @@
+package com.natanbsdev.picpaysimplificado.services;
+
+import com.natanbsdev.picpaysimplificado.domain.user.User;
+import com.natanbsdev.picpaysimplificado.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import javax.management.Notification;
+
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotif(User user, String message ) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+            System.out.println("erro ao enviar a notificação ");
+            throw new Exception("serviço de notificação está fora do ar");
+
+
+
+        }
+    }
+}
