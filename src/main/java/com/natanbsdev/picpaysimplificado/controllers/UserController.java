9505fd075fe1ef037786cf0c5a56cea0b114be85
barbosa-kde <natanbarbosa50@gmail.com>
2024-11-18 18:07:38 -0300

    fix: ajustando para RequestMaping na reque

---
+++ src/main/java/com/natanbsdev/picpaysimplificado/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.natanbsdev.picpaysimplificado.controllers;
+
+import com.natanbsdev.picpaysimplificado.domain.user.User;
+import com.natanbsdev.picpaysimplificado.dtos.UserDTO;
+import com.natanbsdev.picpaysimplificado.services.UserService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/users")
+
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+        User newUser = userService.createUser(userDTO);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+
+}
