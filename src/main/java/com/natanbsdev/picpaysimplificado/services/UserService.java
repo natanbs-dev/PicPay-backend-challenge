08fcf204dc3d01960fc5a1790578a914d77187f2
barbosa-kde <natanbarbosa50@gmail.com>
2024-11-18 17:54:36 -0300

    feat: configurando e ajustando o Service

--- src/main/java/com/natanbsdev/picpaysimplificado/services/UserService.java
+++ src/main/java/com/natanbsdev/picpaysimplificado/services/UserService.java
@@ -1,12 +1,14 @@
 package com.natanbsdev.picpaysimplificado.services;
 
 import com.natanbsdev.picpaysimplificado.domain.user.User;
-import com.natanbsdev.picpaysimplificado.domain.user.userType;
+import com.natanbsdev.picpaysimplificado.domain.user.UserType;
+import com.natanbsdev.picpaysimplificado.dtos.UserDTO;
 import com.natanbsdev.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -15,7 +17,7 @@ public class UserService {
     private UserRepository userRepository;
 
     public void validadeTransaction(User sender, BigDecimal amount) throws Exception {
-        if (sender.getUserType() == userType.MERCHANT) {
+        if (sender.getUserType() == UserType.MERCHANT) {
             throw new Exception("o tipo logista não está autorizado para realizar transação");
         }
         if (sender.getBalance().compareTo(amount) < 0) {
@@ -26,6 +28,16 @@ public class UserService {
     public User findUserById(Long id) throws Exception {
         return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("usuario" +
                 "não encontrado"));
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
     }
 
     public void saveUser(User user) {
