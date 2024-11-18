2c4152423681a5e6dd0e41ae94921dd1a2cfc878
barbosa-kde <natanbarbosa50@gmail.com>
2024-11-18 15:41:28 -0300

    rename: TransactionRepository

--- src/main/java/com/natanbsdev/picpaysimplificado/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.natanbsdev.picpaysimplificado.repository;
-
-import com.natanbsdev.picpaysimplificado.domain.user.transaction.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
